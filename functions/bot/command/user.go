d301889e89f4a3a52efb79173f97cd772f1bd413
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-05-15 05:31:45 -0700

    fix bug

--- functions/bot/command/user.go
+++ functions/bot/command/user.go
@@ -6,12 +6,13 @@ import (
 )
 
 func User(b lib.InteractionBody) error {
+	u := b.Data.Options[0].Value.(string)
 	return b.FollowUp(lib.ResponseData{
 		// todo: user details
 		Embeds: []lib.Embed{
 			{
 				Title: "URL",
-				Url:   fmt.Sprintf("%s/user/%s", lib.GetSiteUrl(), b.Member.User.UserId),
+				Url:   fmt.Sprintf("%s/user/%s", lib.GetSiteUrl(), u),
 			},
 		},
 	})
